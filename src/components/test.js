3fe2587a9b7ed1068f7ece07d0cf860a51e9b937
Marcus Moe <marcusmo1@hotmail.com>
2021-02-16 19:58:39 +0200

    Marcus

---
+++ src/components/test.js
@@ -0,0 +1,13 @@
+
+function test() {
+
+    console.log('Hello from Marcus!');
+
+    return (
+        <div>
+            <h1>Hello from Marcus!</h1>
+        </div>
+    );
+}
+
+export default test;
