7ecd27866a9607038d19a87ed5c7d31f75697c57
Marcus Moe <marcusmo1@hotmail.com>
2021-02-16 20:29:51 +0200

    marcus

---
+++ src/components/Test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Test() {
+
+    for (let i = 0; i < 10; i++) {
+        console.log('Hello from Marcus!');
+    }
+    
+
+    return (
+        <div>
+            <h1>Hello from Marcus!</h1>
+        </div>
+    );
+}
